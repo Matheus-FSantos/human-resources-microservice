72afa58bdfe494d143f6f687c35f26e5717bb4eb
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-09 10:09:26 -0300

    refactor(payroll): update payment service impl package and name

--- hr-payroll/src/main/java/io/github/matheusfsantos/hrpayroll/model/services/impl/PaymentServiceImpl.java
+++ hr-payroll/src/main/java/io/github/matheusfsantos/hrpayroll/model/services/impl/PaymentServiceImpl.java
@@ -1,12 +1,14 @@
-package io.github.matheusfsantos.hrpayroll.model.services;
+package io.github.matheusfsantos.hrpayroll.model.services.impl;
 
 import org.springframework.stereotype.Service;
 
 import io.github.matheusfsantos.hrpayroll.model.entities.Payment;
+import io.github.matheusfsantos.hrpayroll.model.services.PaymentService;
 
 @Service
-public class PaymentService {
+public class PaymentServiceImpl implements PaymentService<Payment> {
 	
+	@Override
 	public Payment getPayment(Long workerId, Integer days) {
 		return new Payment("Matheus", 19.99, 10);
 	}
