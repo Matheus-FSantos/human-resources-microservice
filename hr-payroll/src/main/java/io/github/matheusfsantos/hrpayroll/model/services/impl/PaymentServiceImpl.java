f541179afecf3e820aa707e624131d6b7122321d
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-09 11:01:11 -0300

    refactor(payroll): refactor on payment service impl to work w. open feign

--- hr-payroll/src/main/java/io/github/matheusfsantos/hrpayroll/model/services/impl/PaymentServiceImpl.java
+++ hr-payroll/src/main/java/io/github/matheusfsantos/hrpayroll/model/services/impl/PaymentServiceImpl.java
@@ -1,32 +1,22 @@
 package io.github.matheusfsantos.hrpayroll.model.services.impl;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import io.github.matheusfsantos.hrpayroll.model.dtos.WorkerDTO;
 import io.github.matheusfsantos.hrpayroll.model.entities.Payment;
+import io.github.matheusfsantos.hrpayroll.model.feignClients.WorkerFeignClient;
 import io.github.matheusfsantos.hrpayroll.model.services.PaymentService;
 
 @Service
 public class PaymentServiceImpl implements PaymentService<Payment> {
 	
 	@Autowired
-	private RestTemplate restTemplate;
-	
-	@Value("${hr-worker.host}")
-	private String HR_WORKER_HOST;
+	private WorkerFeignClient workerFeignClient;
 	
 	@Override
-	public Payment getPayment(Long workerId, Integer days) {
-		Map<String, String> uriParams = new HashMap<String, String>();
-		uriParams.put("workerId", workerId + "");
-		
-		WorkerDTO worker = this.restTemplate.getForObject(this.HR_WORKER_HOST.concat("/api/workers/{workerId}"), WorkerDTO.class, uriParams);
+	public Payment getPayment(Long workerId, Integer days) {		
+		WorkerDTO worker = workerFeignClient.findById(workerId).getBody();
 		
 		if(worker != null)
 			return new Payment(worker.getName(), worker.getDailyIncome(), days);
