3c3cea4cf74db9f3660ef1031d26a3654020044c
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-09 12:35:07 -0300

    feat(payroll): add fallbackMethod to handle w. synchronous communication failures

--- hr-payroll/src/main/java/io/github/matheusfsantos/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/io/github/matheusfsantos/hrpayroll/controllers/PaymentController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import io.github.matheusfsantos.hrpayroll.model.entities.Payment;
 import io.github.matheusfsantos.hrpayroll.model.services.impl.PaymentServiceImpl;
 
@@ -17,9 +19,15 @@ public class PaymentController {
 	@Autowired
 	private PaymentServiceImpl service;
 	
+	@HystrixCommand(fallbackMethod="getPaymentAlternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		return ResponseEntity.ok().body(this.service.getPayment(workerId, days));
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("Brann", 400.0, days);
+		return ResponseEntity.ok().body(payment);
 	}
 	
 }
