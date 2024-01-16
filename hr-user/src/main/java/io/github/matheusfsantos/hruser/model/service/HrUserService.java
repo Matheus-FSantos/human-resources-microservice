3619badfbc6a2e8ae06ab7736d22fca05c6fae7a
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-16 10:06:45 -0300

    feat(users): add users service and users service implementation

---
+++ hr-user/src/main/java/io/github/matheusfsantos/hruser/model/service/HrUserService.java
@@ -0,0 +1,8 @@
+package io.github.matheusfsantos.hruser.model.service;
+
+public interface HrUserService<RequestDTO, ResponseDTO> {
+	
+	public ResponseDTO findById(Long id);
+	public ResponseDTO search(String email);
+	
+}
---
+++ hr-user/src/main/java/io/github/matheusfsantos/hruser/model/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package io.github.matheusfsantos.hruser.model.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.github.matheusfsantos.hruser.model.dtos.NewUserDTO;
+import io.github.matheusfsantos.hruser.model.entities.User;
+import io.github.matheusfsantos.hruser.model.repositories.UserRepository;
+import io.github.matheusfsantos.hruser.model.service.HrUserService;
+
+public class UserServiceImpl implements HrUserService<NewUserDTO, User> {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public User findById(Long id) {
+		return this.repository.findById(id).orElse(null);
+	}
+
+	@Override
+	public User search(String email) {
+		return this.repository.findByEmail(email).orNull();
+	}
+
+}
