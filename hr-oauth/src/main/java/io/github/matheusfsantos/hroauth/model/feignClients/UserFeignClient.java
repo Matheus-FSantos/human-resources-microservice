e850e31e2a4197f4467ad1f42db52249d76e47df
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-16 10:35:37 -0300

    chore(users): impl user feign client

--- hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/feignClients/UserFeignClient.java
+++ hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/feignClients/UserFeignClient.java
@@ -1,5 +1,18 @@
 package io.github.matheusfsantos.hroauth.model.feignClients;
 
-public class UserFeignClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import io.github.matheusfsantos.hroauth.model.entities.User;
+
+@Component
+@FeignClient(name="hr-user", path="/api/users")
+public interface UserFeignClient {
+
+	@GetMapping("/search")
+	public ResponseEntity<User> search(@RequestParam(name="email") String email);
+		
 }
