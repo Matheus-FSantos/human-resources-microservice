7604cd3f00fdfbad3494bb17620a45c13c4cda4a
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-17 11:32:48 -0300

    chore(config): add jwt secret token configuration

--- hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/config/AppConfig.java
+++ hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/config/AppConfig.java
@@ -1,5 +1,6 @@
 package io.github.matheusfsantos.hroauth.model.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	public String jwtSecret;
+	
 	@Bean
 	BCryptPasswordEncoder getBCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -19,7 +23,7 @@ public class AppConfig {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
 		
 		tokenConverter.
-			setSigningKey("MY-SECRET-KEY");
+			setSigningKey(jwtSecret);
 		
 		return tokenConverter;
 	}
