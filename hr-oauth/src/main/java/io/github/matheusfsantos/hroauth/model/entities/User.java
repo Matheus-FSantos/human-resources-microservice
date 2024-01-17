bcfffb5fdfb4a7f6d91980a0550a9936196d3346
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-17 09:21:50 -0300

    chore(oauth): implements user details interface (spring sec. specification)

--- hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/entities/User.java
+++ hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/entities/User.java
@@ -2,11 +2,17 @@ package io.github.matheusfsantos.hroauth.model.entities;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User implements Serializable {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails, Serializable {
 	
 	public static final long serialVersionUID = 1L;
 	
@@ -141,6 +147,39 @@ public class User implements Serializable {
 	
 	public Set<Role> getRoles() {
 		return roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles
+				.stream()
+					.map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+					.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 }
