c86d4f659aeab966bff82b9d0983bfe6aa87a1a4
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-17 09:09:45 -0300

    chore(dependency-injection)

---
+++ hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/config/AppConfig.java
@@ -0,0 +1,32 @@
+package io.github.matheusfsantos.hroauth.model.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	BCryptPasswordEncoder getBCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	JwtAccessTokenConverter getJwtAccessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		
+		tokenConverter.
+			setSigningKey("My-Secret-Key");
+		
+		return tokenConverter;
+	}
+	
+	@Bean
+	JwtTokenStore getJwtTokenStore() {
+		return new JwtTokenStore(getJwtAccessTokenConverter());
+	}
+	
+}
