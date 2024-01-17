a22934c707e2878ef156fd412589f12d51a97611
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-17 11:33:09 -0300

    chore(config): add oauth app name/secret configuration

--- hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package io.github.matheusfsantos.hroauth.model.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+	@Value("${oauth.client.name}")
+	public String oauthClientName;
+	
+	@Value("${oauth.client.secret}")
+	public String oauthClientSecret;
+	
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
@@ -39,8 +46,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients
 			.inMemory()
-				.withClient("myappname123")
-				.secret(bCryptPasswordEncoder.encode("myappsecret123"))
+				.withClient(oauthClientName)
+				.secret(bCryptPasswordEncoder.encode(oauthClientSecret))
 				.scopes("read", "write")
 				.authorizedGrantTypes("password")
 				.accessTokenValiditySeconds(86400); /* 24hrs */
