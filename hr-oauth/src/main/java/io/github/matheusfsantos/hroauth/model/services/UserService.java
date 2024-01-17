72ad97e028051c4af5972275299389bdf2f9f86b
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-17 09:25:02 -0300

    chore(oauth): implements UserDetailsService on UserService class

--- hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/services/UserService.java
+++ hr-oauth/src/main/java/io/github/matheusfsantos/hroauth/model/services/UserService.java
@@ -3,13 +3,16 @@ package io.github.matheusfsantos.hroauth.model.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import io.github.matheusfsantos.hroauth.model.entities.User;
 import io.github.matheusfsantos.hroauth.model.feignClients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -22,9 +25,21 @@ public class UserService {
 		if(user == null) {
 			UserService.logger.error("E-mail not found: " + email);
 			throw new IllegalArgumentException("User not found");
-		}
-		else {
+		} else {
 			UserService.logger.info("E-mail found: " + email);
+			return user;
+		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = this.userFeignClient.search(username).getBody();
+		
+		if(user == null) {
+			UserService.logger.error("E-mail not found: " + username);
+			throw new UsernameNotFoundException("User not found");
+		} else {
+			UserService.logger.info("E-mail found: " + username);
 			return user;
 		}
 	}
